0fe036be12e5b1137613156630487e3755335858
rupweb <rupert@webstersystems.co.uk>
2024-02-01 16:42:16 +0000

    Added header and footer

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,4 +2,22 @@ function helloWorld() {
   return "Hello world!";
   }
 
-export { helloWorld };
+document.addEventListener('DOMContentLoaded', function () {
+  // Fetch header
+  fetch('header.html')
+      .then(response => response.text())
+      .then(content => {
+          document.getElementById('header-container').innerHTML = content;
+      })
+      .catch(error => console.error('Error loading header:', error));
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Fetch footer
+  fetch('footer.html')
+      .then(response => response.text())
+      .then(content => {
+          document.getElementById('footer-container').innerHTML = content;
+      })
+      .catch(error => console.error('Error loading footer:', error));
+});
